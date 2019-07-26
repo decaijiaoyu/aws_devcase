fa801d60e00e63b72f3d4d0632b940672b0c069b
lllluobo <37678425+lllluobo@users.noreply.github.com>
2019-07-26 16:00:49 +0800

    Update Plugins.java

--- com.awspaas.user.apps.devcase/src/com/awspaas/user/apps/devcase/Plugins.java
+++ com.awspaas.user.apps.devcase/src/com/awspaas/user/apps/devcase/Plugins.java
@@ -2,9 +2,12 @@ package com.awspaas.user.apps.devcase;
 
 import com.actionsoft.apps.listener.PluginListener;
 import com.actionsoft.apps.resource.AppContext;
+import com.actionsoft.apps.resource.plugin.profile.ASLPPluginProfile;
 import com.actionsoft.apps.resource.plugin.profile.AWSPluginProfile;
+import com.actionsoft.apps.resource.plugin.profile.HttpASLP;
 import com.actionsoft.apps.resource.plugin.profile.SkinsPluginProfile;
 import com.awspaas.user.apps.devcase.metroskins.MetroSkinsDemo;
+import com.awspaas.user.apps.devcase.tempUser.aslp.AslpApplyTempUser;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +21,9 @@ public class Plugins implements PluginListener {
         List<AWSPluginProfile> list = new ArrayList<AWSPluginProfile>();
         // 注册门户主题风格
         list.add(new SkinsPluginProfile(MetroSkinsDemo.class.getName(), false));
+
+        // 临时用户申请接口
+        list.add(new ASLPPluginProfile("applyTempUser", AslpApplyTempUser.class.getName(),"外部申请临时账号",new HttpASLP(HttpASLP.AUTH_KEY,"AwsDev")));
         return list;
     }
 }
