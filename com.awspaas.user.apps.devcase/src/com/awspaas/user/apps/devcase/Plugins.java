e253066c76524e0704efabf09e41345819950934
lllluobo <37678425+lllluobo@users.noreply.github.com>
2019-07-31 15:22:32 +0800

    Add files via upload

--- com.awspaas.user.apps.devcase/src/com/awspaas/user/apps/devcase/Plugins.java
+++ com.awspaas.user.apps.devcase/src/com/awspaas/user/apps/devcase/Plugins.java
@@ -7,6 +7,7 @@ import com.actionsoft.apps.resource.plugin.profile.AWSPluginProfile;
 import com.actionsoft.apps.resource.plugin.profile.HttpASLP;
 import com.actionsoft.apps.resource.plugin.profile.SkinsPluginProfile;
 import com.awspaas.user.apps.devcase.metroskins.MetroSkinsDemo;
+import com.awspaas.user.apps.devcase.notice.aslp.AslpCreateNoticeProcess;
 import com.awspaas.user.apps.devcase.tempUser.aslp.AslpApplyTempUser;
 
 import java.util.ArrayList;
@@ -24,6 +25,9 @@ public class Plugins implements PluginListener {
 
         // 临时用户申请接口
         list.add(new ASLPPluginProfile("applyTempUser", AslpApplyTempUser.class.getName(),"外部申请临时账号",new HttpASLP(HttpASLP.AUTH_KEY,"AwsDev")));
+        // 信息发布接口
+        list.add(new ASLPPluginProfile("applyNotice", AslpCreateNoticeProcess.class.getName(),"信息发布",new HttpASLP(HttpASLP.AUTH_KEY,"AwsDev")));
+
         return list;
     }
 }
